8baefc9b04818020e6458b182ca0e1309e9aafc4
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2025-06-05 23:09:13 +0300

    Add pages.h with template path definitions

---
+++ headers/pages.h
@@ -0,0 +1,18 @@
+//
+//  pages.h
+//  CServer
+//
+//  Created by ibrahim alnakeeb on 21/01/2023.
+//
+
+#ifndef pages_h
+#define pages_h
+
+
+#define _400_PAGE "assets/templates/400.html"
+#define _404_PAGE "assets/templates/404.html"
+#define HOME_PAGE "assets/templates/index.html"
+#define LOGIN_PAGE "assets/templates/login.html"
+
+
+#endif /* pages_h */
