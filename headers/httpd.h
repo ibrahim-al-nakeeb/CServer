f7210cc80635fdf118eea1ee16473add2283bb22
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2025-06-21 15:22:22 +0300

    convert spaces to tabs in httpd.h

--- headers/httpd.h
+++ headers/httpd.h
@@ -17,29 +17,29 @@ void serve_forever(const char *PORT);
 
 // Client request
 
-extern char     *method,         // "GET" or "POST"
-                *uri,            // "/index.html" things before '?'
-                *qs,             // "a=1&b=2"     things after  '?'
-                *prot,           // "HTTP/1.1"
-                *payload;        // for POST
-extern int      payload_size;
+extern char		*method,         // "GET" or "POST"
+				*uri,            // "/index.html" things before '?'
+				*qs,             // "a=1&b=2"     things after  '?'
+				*prot,           // "HTTP/1.1"
+				*payload;        // for POST
+extern int		payload_size;
 
 char *request_header(const char* name);
 
 void route();
 
 // some interesting macro for `route()`
-#define ROUTE_START()       if (0) {
-#define ROUTE(METHOD,URI)   } else if (strcmp(URI,uri)==0&&strcmp(METHOD,method)==0) {
-#define ROUTE_GET(URI)      ROUTE("GET", URI)
-#define ROUTE_POST(URI)     ROUTE("POST", URI)
+#define ROUTE_START()		if (0) {
+#define ROUTE(METHOD,URI)	} else if (strcmp(URI,uri)==0&&strcmp(METHOD,method)==0) {
+#define ROUTE_GET(URI)		ROUTE("GET", URI)
+#define ROUTE_POST(URI)		ROUTE("POST", URI)
 #define ROUTE_GET_STARTS_WITH(PREFIX) \
-                            } else if (strncmp(uri, PREFIX, strlen(PREFIX)) == 0 && strcmp(method, "GET") == 0) {
+							} else if (strncmp(uri, PREFIX, strlen(PREFIX)) == 0 && strcmp(method, "GET") == 0) {
 
-#define ROUTE_END()         } else printf(\
-                                "HTTP/1.1 500 Not Handled\r\n\r\n" \
-                                "The server has no handler to the request.\r\n" \
-                            );
+#define ROUTE_END()			} else printf(\
+								"HTTP/1.1 500 Not Handled\r\n\r\n" \
+								"The server has no handler to the request.\r\n" \
+							);
 
 
 #endif /* httpd_h */
